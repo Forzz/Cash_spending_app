156c0c6c7b8d78becd51d29021ff408d3eb8aac1
Forzz <bannmaxx2000@yandex.ru>
2020-04-17 17:55:50 +0300

    Added titles output of transactions

--- lib/main.dart
+++ lib/main.dart
@@ -21,32 +21,44 @@ class MyHomePage extends StatelessWidget {
       title: 'New Shoes',
       amount: 69.99,
       date: DateTime.now(),
-    )
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Cool Hat',
+      amount: 23.55,
+      date: DateTime.now(),
+    ),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter app'),
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Container(
-              width: double.infinity,
-              child: Card(
-                color: Colors.blue,
-                child: Text('CHART'),
-                elevation: 5,
-              ),
+      appBar: AppBar(
+        title: Text('Flutter app'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              child: Text('CHART'),
+              elevation: 5,
             ),
-            Card(
-              color: Colors.red,
-              child: Text('LIST OF TEXT'),
-            ),
-          ],
-        ));
+          ),
+          Column(
+            //Transforming list of objects to list of widgets
+            children: transactions.map((tx) {
+              //Returnin widget Card with title text from transactions
+              return Card(
+                child: Text(tx.title),
+              );
+            }).toList(),
+          ),
+        ],
+      ),
+    );
   }
 }
