adeaf0c2709852907792fa49d46f948f6f50e7a0
Forzz <bannmaxx2000@yandex.ru>
2020-04-22 14:37:06 +0300

    Button now adding new transaction in list below

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,42 +1,48 @@
 import 'package:flutter/material.dart';
 
 class NewTransaction extends StatelessWidget {
+  final Function addTx;
   final _titleController = TextEditingController();
-  final _amountController = TextEditingController(); 
+  final _amountController = TextEditingController();
+
+  NewTransaction(this.addTx);
 
   @override
   Widget build(BuildContext context) {
     return Card(
-            elevation: 5,
-            child: Container(
-              padding: EdgeInsets.all(10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: <Widget>[
-                  TextField(
-                    decoration: InputDecoration(labelText: 'Title'),
-                    controller: _titleController,
-                    // onChanged: (value) {
-                    //   titleInput = value;
-                    // },
-                  ),
-                  TextField(
-                    decoration: InputDecoration(labelText: 'Amount'),
-                    controller: _amountController,
-                    // onChanged: (value) {
-                    //   amountInput = value;
-                    // },
-                  ),
-                  FlatButton(
-                    child: Text('Add Transaction'),
-                    onPressed: () {
-                      print('${_titleController.text} ${_amountController.text}');
-                    },
-                    textColor: Colors.red[800],
-                  )
-                ],
-              ),
+      elevation: 5,
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(labelText: 'Title'),
+              controller: _titleController,
+              // onChanged: (value) {
+              //   titleInput = value;
+              // },
             ),
-          );
+            TextField(
+              decoration: InputDecoration(labelText: 'Amount'),
+              controller: _amountController,
+              // onChanged: (value) {
+              //   amountInput = value;
+              // },
+            ),
+            FlatButton(
+              child: Text('Add Transaction'),
+              onPressed: () {
+                addTx(
+                  _titleController.text,
+                  double.parse(_amountController.text),
+                );
+              },
+              textColor: Colors.red[800],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
