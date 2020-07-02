099cba6fb63dffa268e62d8322bad6e1188ed4af
Forzz <bannmaxx2000@yandex.ru>
2020-07-02 20:26:32 +0300

    bkt fix

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -12,54 +12,53 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 535,
-        child: ListView.builder(
-          itemBuilder: (ctx, index) {
-            //Returnin widget Card with amount, title and date text from transactions
-            return Card(
-              child: Row(
-                children: <Widget>[
-                  Container(
-                    margin: const EdgeInsets.symmetric(
-                      vertical: 10,
-                      horizontal: 15,
+      child: ListView.builder(
+        itemBuilder: (ctx, index) {
+          //Returnin widget Card with amount, title and date text from transactions
+          return Card(
+            child: Row(
+              children: <Widget>[
+                Container(
+                  margin: const EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 15,
+                  ),
+                  decoration: BoxDecoration(
+                      border: Border.all(
+                    color: Theme.of(context).primaryColor,
+                    width: 2,
+                  )),
+                  padding: const EdgeInsets.all(10),
+                  child: Text(
+                    '\$${transactions[index].amount.toStringAsFixed(2)}',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Theme.of(context).primaryColor),
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      transactions[index].title,
+                      style: Theme.of(context).textTheme.headline6,
                     ),
-                    decoration: BoxDecoration(
-                        border: Border.all(
-                      color: Theme.of(context).primaryColor,
-                      width: 2,
-                    )),
-                    padding: const EdgeInsets.all(10),
-                    child: Text(
-                      '\$${transactions[index].amount.toStringAsFixed(2)}',
+                    Text(
+                      DateFormat.yMMMMd().format(transactions[index].date),
                       style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: Theme.of(context).primaryColor),
+                        color: Colors.grey[700],
+                      ),
                     ),
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        transactions[index].title,
-                        style: Theme.of(context).textTheme.headline6,
-                        ),
-                      ),
-                      Text(
-                        DateFormat.yMMMMd().format(transactions[index].date),
-                        style: TextStyle(
-                          color: Colors.grey[700],
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            );
-          },
-          itemCount: transactions.length,
-          //Transforming list of objects to list of widgets
-        ),
+                  ],
+                ),
+              ],
+            ),
+          );
+        },
+        itemCount: transactions.length,
+        //Transforming list of objects to list of widgets
+      ),
     );
   }
 }
